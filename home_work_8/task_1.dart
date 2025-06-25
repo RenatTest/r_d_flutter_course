bd0d6fa6af16c1e970bb352eb2a22e426db15cc1
RenatTest <renattesttask@gmail.com>
2025-06-25 19:10:19 +0300

    h_w_8 task_1

--- home_work_8/task_1.dart
+++ home_work_8/task_1.dart
@@ -6,4 +6,17 @@ void main() {
   // Видаліть одну з мов.
   // Виведіть список у консоль по одному елементу з індексом. (1- Dart, 2-....)
   // Виведіть список в алфавітному порядку.
+
+  List<String> langs = ['Dart', 'JavaScript', 'Python', 'Java', 'Go'];
+  langs.add('Swift');
+  langs.remove('Go');
+
+  for (var i = 0; i < langs.length; i++) {
+    print('${i + 1} - ${langs[i]}');
+  }
+
+  langs.sort();
+  for (final el in langs) {
+    print(el);
+  }
 }
