b810bb83dcf78aeb6875d0fbab585edf1d415468
RenatTest <renattesttask@gmail.com>
2025-07-11 13:26:41 +0300

    feat(RD-GO_ROUTER): test go router in app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: depend_on_referenced_packages
 
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:r_d_flutter_course/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -12,34 +13,46 @@ void main() async {
   runApp(const MyApp());
 }
 
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return HomePage();
+      },
+    ),
+    GoRoute(
+      path: '/settings',
+      builder: (BuildContext context, GoRouterState state) {
+        return const SettingsPage();
+      },
+    ),
+  ],
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp.router(routerConfig: _router);
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  bool isLoading = false;
 
   Future<bool> logInWithGoogle() async {
+    setState(() {
+      isLoading = true;
+    });
     final user = await GoogleSignIn().signIn();
     GoogleSignInAuthentication userAuth = await user!.authentication;
     var credential = GoogleAuthProvider.credential(
@@ -47,49 +60,50 @@ class _MyHomePageState extends State<MyHomePage> {
       accessToken: userAuth.accessToken,
     );
     await FirebaseAuth.instance.signInWithCredential(credential);
-    return FirebaseAuth.instance.currentUser != null;
-  }
-
-  void _incrementCounter() {
     setState(() {
-      _counter++;
+      isLoading = false;
     });
+    return FirebaseAuth.instance.currentUser != null;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: Text('HomePage')),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                bool isLogged = await logInWithGoogle();
+        child: isLoading
+            ? CircularProgressIndicator()
+            : ElevatedButton(
+                onPressed: () async {
+                  bool isLogged = await logInWithGoogle();
 
-                if (isLogged) {
-                  print('Logged in with GOOGLE - success!');
-                }
-              },
-              child: Text('Sign in with Google'),
-            ),
-          ],
-        ),
+                  if (isLogged) {
+                    print('Logged in with GOOGLE - success!');
+                    // ignore: use_build_context_synchronously
+                    context.go('/settings');
+                  }
+                },
+                child: Text('Sign in with Google'),
+              ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Google sign in',
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('SettingsPage')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            context.go('/');
+          },
+          child: Text('Go back to the HomePage'),
+        ),
       ),
     );
   }
