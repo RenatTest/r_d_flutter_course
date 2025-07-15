5a2440b62be458fb79ccfbb44395df1330c03bae
RenatTest <renattesttask@gmail.com>
2025-07-15 11:15:28 +0300

    feat(LAB-13-widgets-p1): added materials from lesson

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,82 @@
-// ignore_for_file: depend_on_referenced_packages
-
-// import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:r_d_flutter_course/firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:r_d_flutter_course/router/router.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+import 'features/widgets/presentation/screens/widgets_main_screen.dart';
+
+void main() {
+  runApp(const FlutterWidgetsApp());
 }
 
-final GoRouter _router = GoRouter(
-  routes: <RouteBase>[
-    GoRoute(
-      path: '/',
-      builder: (BuildContext context, GoRouterState state) {
-        return HomePage();
-      },
-    ),
-    GoRoute(
-      path: '/settings',
-      builder: (BuildContext context, GoRouterState state) {
-        return const SettingsPage();
-      },
-    ),
-  ],
-);
-
-// final _appRouter = AppRouter();
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FlutterWidgetsApp extends StatelessWidget {
+  const FlutterWidgetsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(routerConfig: _router);
+    return const MaterialApp(home: HomeScreen());
   }
 }
 
-// @RoutePage()
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  bool isLoading = false;
-
-  Future<bool> logInWithGoogle() async {
-    setState(() {
-      isLoading = true;
-    });
-    final user = await GoogleSignIn().signIn();
-    GoogleSignInAuthentication userAuth = await user!.authentication;
-    var credential = GoogleAuthProvider.credential(
-      idToken: userAuth.idToken,
-      accessToken: userAuth.accessToken,
-    );
-    await FirebaseAuth.instance.signInWithCredential(credential);
-    setState(() {
-      isLoading = false;
-    });
-    return FirebaseAuth.instance.currentUser != null;
-  }
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('HomePage')),
-      body: Center(
-        child: isLoading
-            ? CircularProgressIndicator()
-            : ElevatedButton(
-                onPressed: () async {
-                  bool isLogged = await logInWithGoogle();
-
-                  if (isLogged) {
-                    print('Logged in with GOOGLE - success!');
-                    // ignore: use_build_context_synchronously
-                    context.go('/settings');
-                    // ignore: use_build_context_synchronously
-                    // AutoRouter.of(context).push(SettingsRoute());
-                  }
-                },
-                child: Text('Sign in with Google'),
+      appBar: AppBar(
+        title: const Text('Flutter Lab'),
+        backgroundColor: Colors.blue.shade100,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            FeatureCard(
+              title: 'Widgets',
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute<Widget>(
+                  builder: (context) => const WidgetsScreen(),
+                ),
               ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
-// @RoutePage()
-class SettingsPage extends StatelessWidget {
-  const SettingsPage({super.key});
+class FeatureCard extends StatelessWidget {
+  const FeatureCard({required this.title, required this.onTap, super.key});
+
+  final String title;
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('SettingsPage')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            context.go('/');
-            // AutoRouter.of(context).push(HomeRoute());
-          },
-          child: Text('Go back to the HomePage'),
+    return Card(
+      elevation: 2,
+      child: InkWell(
+        onTap: onTap,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Icon(
+                Icons.arrow_forward_ios,
+                size: 16,
+                color: Colors.grey.shade600,
+              ),
+            ],
+          ),
         ),
       ),
     );
