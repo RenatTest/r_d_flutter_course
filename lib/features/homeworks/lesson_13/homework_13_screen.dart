2123cfd68e17d5e3c5b531ea5b11ecba79b2d6c7
RenatTest <renattesttask@gmail.com>
2025-07-15 13:29:31 +0300

    feat(LAB-13-homework): Subtask 5 (added SingleChildScrollView for small displays)

--- lib/features/homeworks/lesson_13/homework_13_screen.dart
+++ lib/features/homeworks/lesson_13/homework_13_screen.dart
@@ -10,79 +10,80 @@ class Homework13Screen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Homework_13')),
       body: Center(
-        child: Column(
-          spacing: 8,
-          children: [
-            Expanded(
-              child: MyContainer(
-                color: Colors.blue,
-                alignment: Alignment.topLeft,
-              ),
-            ),
-            MyContainer(color: Colors.green, alignment: Alignment.center),
-            MyContainer(color: Colors.red, alignment: Alignment.bottomRight),
-            Stack(
-              children: [
-                Container(color: Colors.grey, height: 150, width: screenWidth),
-                Positioned(
-                  top: 20,
-                  left: 20,
-                  child: Container(
-                    color: Colors.deepOrange,
-                    height: 50,
-                    width: 100,
+        child: SingleChildScrollView(
+          child: Column(
+            spacing: 8,
+            children: [
+              MyContainer(color: Colors.blue, alignment: Alignment.topLeft),
+              MyContainer(color: Colors.green, alignment: Alignment.center),
+              MyContainer(color: Colors.red, alignment: Alignment.bottomRight),
+              Stack(
+                children: [
+                  Container(
+                    color: Colors.grey,
+                    height: 150,
+                    width: screenWidth,
                   ),
-                ),
-                Positioned(
-                  top: 40,
-                  left: 90,
-                  child: Text(
-                    'STACK',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w900,
+                  Positioned(
+                    top: 20,
+                    left: 20,
+                    child: Container(
+                      color: Colors.deepOrange,
+                      height: 50,
+                      width: 100,
                     ),
                   ),
-                ),
-                Positioned(
-                  bottom: 20,
-                  right: 20,
-                  child: IconButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    icon: const Icon(Icons.exit_to_app),
-                    color: Colors.lightGreenAccent,
-                    iconSize: 50,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 100,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return Container(
-                    width: 100,
-                    decoration: BoxDecoration(
-                      color: Colors.lightGreen,
-                      borderRadius: BorderRadius.circular(100),
-                    ),
-                    child: Center(
-                      child: CircularProgressIndicator(
-                        color: Colors.deepPurpleAccent,
-                        backgroundColor: Colors.deepOrange,
-                        strokeWidth: 10,
+                  Positioned(
+                    top: 40,
+                    left: 90,
+                    child: Text(
+                      'STACK',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w900,
                       ),
                     ),
-                  );
-                },
+                  ),
+                  Positioned(
+                    bottom: 20,
+                    right: 20,
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: const Icon(Icons.exit_to_app),
+                      color: Colors.lightGreenAccent,
+                      iconSize: 50,
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
+              SizedBox(
+                height: 100,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.lightGreen,
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      child: Center(
+                        child: CircularProgressIndicator(
+                          color: Colors.deepPurpleAccent,
+                          backgroundColor: Colors.deepOrange,
+                          strokeWidth: 10,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
