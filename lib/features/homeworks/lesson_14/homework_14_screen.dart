2c830869c797d48853d59fa17526615e2b12f838
RenatTest <renattesttask@gmail.com>
2025-07-18 10:18:08 +0300

    feat(LAB-14-widgets-p2): added materials from lesson

---
+++ lib/features/homeworks/lesson_14/homework_14_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Homework14Screen extends StatelessWidget {
+  const Homework14Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Homework_14'),
+      ),
+      body: const SizedBox.shrink(),
+    );
+  }
+}
