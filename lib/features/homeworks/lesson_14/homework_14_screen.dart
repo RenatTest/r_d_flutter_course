d9b127e96786f5a429949f13efa398527a6c7b35
RenatTest <renattesttask@gmail.com>
2025-07-18 15:44:26 +0300

    feat(LAB-14-homework): added push like-dislike

--- lib/features/homeworks/lesson_14/homework_14_screen.dart
+++ lib/features/homeworks/lesson_14/homework_14_screen.dart
@@ -211,10 +211,36 @@ class ItemContainer extends StatelessWidget {
   }
 }
 
-class ItemRowContainer extends StatelessWidget {
+class ItemRowContainer extends StatefulWidget {
   const ItemRowContainer({required this.rowTitle, super.key});
 
   final String rowTitle;
+
+  @override
+  State<ItemRowContainer> createState() => _ItemRowContainerState();
+}
+
+class _ItemRowContainerState extends State<ItemRowContainer> {
+  bool isLikePushed = false;
+  bool isDislikePushed = false;
+
+  void _addLike() {
+    setState(() {
+      isLikePushed = !isLikePushed;
+      if (isLikePushed) {
+        isDislikePushed = false;
+      }
+    });
+  }
+
+  void _addDislike() {
+    setState(() {
+      isDislikePushed = !isDislikePushed;
+      if (isDislikePushed) {
+        isLikePushed = false;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -228,22 +254,32 @@ class ItemRowContainer extends StatelessWidget {
       child: Row(
         children: [
           TextWidget(
-            text: rowTitle,
+            text: widget.rowTitle,
             size: 16,
             weight: FontWeight.w400,
             color: HexColor('#13131e'),
           ),
           Expanded(child: SizedBox(height: 40)),
-          Image.asset(
-            'assets/images/dislike-not-active.png',
-            width: 40,
-            height: 40,
+          GestureDetector(
+            onTap: _addDislike,
+            child: Image.asset(
+              isDislikePushed
+                  ? 'assets/images/dislike-active.png'
+                  : 'assets/images/dislike-not-active.png',
+              width: 40,
+              height: 40,
+            ),
           ),
           SizedBox(width: 12),
-          Image.asset(
-            'assets/images/like-not-active.png',
-            width: 40,
-            height: 40,
+          GestureDetector(
+            onTap: _addLike,
+            child: Image.asset(
+              isLikePushed
+                  ? 'assets/images/like-active.png'
+                  : 'assets/images/like-not-active.png',
+              width: 40,
+              height: 40,
+            ),
           ),
         ],
       ),
