158bfbe4b3b78c26eacdd3fce5b01e14d49100ea
RenatTest <renattesttask@gmail.com>
2025-07-24 14:12:58 +0300

    feat(LAB-15-constrainst-homework): start

---
+++ lib/features/homeworks/lesson_15/homework_15_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Homework14Screen extends StatelessWidget {
+  const Homework14Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
