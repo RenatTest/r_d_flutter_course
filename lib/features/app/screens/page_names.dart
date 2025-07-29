675cd886100b742286c892fa26d961d1ef58367a
RenatTest <renattesttask@gmail.com>
2025-07-29 20:50:22 +0300

    feat(LAB-17-go-router-homework): added page names

--- lib/features/app/screens/page_names.dart
+++ lib/features/app/screens/page_names.dart
@@ -1,3 +1,59 @@
 abstract class ScreenNames {
   static const home = 'home';
+
+  static const widgets = 'widgets';
+  static const widgetsFirstPart = 'widgets-first-part';
+  // Widgets part 1
+  static const containerExample = 'container';
+  static const rowExample = 'row';
+  static const columnExample = 'column';
+  static const expandedExample = 'expanded';
+  static const stackExample = 'stack';
+  static const sizedBoxExample = 'sized-box';
+  static const paddingExample = 'padding';
+  static const alignExample = 'align';
+  static const centerExample = 'center';
+  static const textExample = 'text';
+  static const buttonsExample = 'buttons';
+  static const scrollExample = 'scroll';
+  static const progressIndicatorsExample = 'progress-indicators';
+
+  static const widgetsSecondPart = 'widgets-second-part';
+  // Widgets part 2
+  static const customWidgetsExample = 'custom-widgets';
+  static const widgetTypesExample = 'widget-types';
+  static const gestureExample = 'gesture';
+  static const textFieldExample = 'text-field';
+  static const imagesExample = 'images';
+
+  static const understandingConstraints = 'understanding-constraints';
+  // Understanding constraints
+  static const understandingConstraintsExample1 =
+      'understanding-constraints-example-1';
+  static const understandingConstraintsExample2 =
+      'understanding-constraints-example-2';
+  static const understandingConstraintsExample3 =
+      'understanding-constraints-example-3';
+  static const understandingConstraintsExample4 =
+      'understanding-constraints-example-4';
+  static const understandingConstraintsExample5 =
+      'understanding-constraints-example-5';
+
+  static const homework13 = 'homework-13';
+  static const homework14 = 'homework-14';
+  static const homework15 = 'homework-15';
+
+  static const navigation = 'navigation';
+  // Navigation
+  static const baseNavigation = 'base-navigation';
+  static const simpleEmptyScreen = 'simple-empty';
+  static const simpleScreenWithData = 'simple-with-data';
+  static const simpleScreenWithReturningData = 'simple-with-returning-data';
+  static const simpleEmptyScreenWithCustomTransition =
+      'simple-empty-with-custom-transition';
+  static const simpleTransitionWithArguments =
+      'simple-transition-with-arguments';
+
+  static const namedRoutesNavigation = 'named-routes-navigation';
+  static const homework16 = 'homework-16';
 }
