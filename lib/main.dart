96e877618fc9d2a08e0d03b1223c1736b15e4469
RenatTest <renattesttask@gmail.com>
2025-08-04 13:19:03 +0300

    feat(LAB-18-state-management-homework): added auth_provider with GoogleSignIn() for iOS/android

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:r_d_flutter_course/features/state_managment/providers/auth_provider.dart';
 import 'package:r_d_flutter_course/features/state_managment/providers/counter_provider.dart';
+import 'package:r_d_flutter_course/firebase_options.dart';
 import 'package:r_d_flutter_course/router/app_router.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const FlutterWidgetsApp());
 }
 
@@ -12,8 +17,11 @@ class FlutterWidgetsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => CounterProvider(22),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => CounterProvider(22)),
+        ChangeNotifierProvider(create: (context) => AuthProvider()),
+      ],
       child: MaterialApp.router(routerConfig: router),
     );
   }
