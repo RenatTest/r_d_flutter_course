f8e71b9d03b256dd43c5b1411c98d29a6ff7aed4
RenatTest <renattesttask@gmail.com>
2025-08-05 10:09:45 +0300

    feat(LAB-19-BLOC-intro-homework): added HomeworkCubitScreen and HomeworkBlocScreen

---
+++ lib/features/homeworks/lesson_19/homework_bloc/homework_bloc_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomeworkBlocScreen extends StatefulWidget {
+  const HomeworkBlocScreen({super.key});
+
+  @override
+  State<HomeworkBlocScreen> createState() => _HomeworkBlocScreenState();
+}
+
+class _HomeworkBlocScreenState extends State<HomeworkBlocScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Homework Bloc Screen')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/features/homeworks/lesson_19/homework_сubit/homework_cubit_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomeworkCubitScreen extends StatefulWidget {
+  const HomeworkCubitScreen({super.key});
+
+  @override
+  State<HomeworkCubitScreen> createState() => _HomeworkCubitScreenState();
+}
+
+class _HomeworkCubitScreenState extends State<HomeworkCubitScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Homework Cubit Screen')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
