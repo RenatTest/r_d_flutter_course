9e252f365f44cfd2cfff8815fc07df452fb64454
RenatTest <renattesttask@gmail.com>
2025-08-11 16:47:35 +0300

    feat(LAB-20-BLOC-widgets): Using Equatable to Compare the values of objects by their parameters

--- lib/features/homeworks/lesson_19/homework_сubit/cubit/counter_state.dart
+++ lib/features/homeworks/lesson_19/homework_сubit/cubit/counter_state.dart
@@ -1,9 +1,31 @@
-class CounterCubitState {
-  const CounterCubitState({required this.counter});
+import 'package:equatable/equatable.dart';
+
+class CounterCubitState extends Equatable {
+  const CounterCubitState({
+    required this.counter,
+    required this.favoriteNumber,
+  });
 
   final int counter;
+  final int favoriteNumber;
 
-  CounterCubitState copyWith({int? counter}) {
-    return CounterCubitState(counter: counter ?? this.counter);
+  CounterCubitState copyWith({int? counter, int? favoriteNumber}) {
+    return CounterCubitState(
+      counter: counter ?? this.counter,
+      favoriteNumber: favoriteNumber ?? this.favoriteNumber,
+    );
   }
+
+  @override
+  List<Object?> get props => [counter, favoriteNumber];
+}
+
+void main() {
+  final state1 = CounterCubitState(counter: 3, favoriteNumber: 22);
+  final state2 = CounterCubitState(counter: 3, favoriteNumber: 22);
+
+  print(state1.hashCode); // 360273621
+  print(state2.hashCode); // 731477553
+
+  print(state1 == state2); // false
 }
