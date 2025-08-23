6c21d6f26a3c2aca7084cd31b9742e1c42f7bafe
RenatTest <renattesttask@gmail.com>
2025-08-23 13:23:25 +0300

    feat(LAB-22-explicit-animations-homework): added simple animation with rotation and changing ball position

--- lib/features/homeworks/lesson_22_explicit_animations/homework_animations_screen.dart
+++ lib/features/homeworks/lesson_22_explicit_animations/homework_animations_screen.dart
@@ -1,8 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
-class Homework22Screen extends StatelessWidget {
+class Homework22Screen extends StatefulWidget {
   const Homework22Screen({super.key});
+
+  @override
+  State<Homework22Screen> createState() => _Homework22ScreenState();
+}
+
+class _Homework22ScreenState extends State<Homework22Screen>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late final Animation<Alignment> _alignmentAnimation;
+  late final Animation<double> _rotationAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(
+          duration: const Duration(milliseconds: 700),
+          vsync: this,
+        )..addListener(() {
+          if (_controller.status == AnimationStatus.completed) {
+            _move2();
+          }
+        });
+
+    _alignmentAnimation = AlignmentTween(
+      begin: Alignment.bottomCenter,
+      end: Alignment.topCenter,
+    ).animate(_controller);
+
+    _rotationAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _move() {
+    _controller.forward();
+  }
+
+  void _move2() {
+    _controller.reverse();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,10 +69,22 @@ class Homework22Screen extends StatelessWidget {
               height: MediaQuery.of(context).size.height,
               color: HexColor('#90cdfa'),
               alignment: Alignment.bottomCenter,
-              child: Image.asset(
-                width: 100,
-                height: 100,
-                'assets/images/ball.png',
+              child: GestureDetector(
+                onTap: _move,
+                child: AnimatedBuilder(
+                  animation: _controller,
+                  builder: (context, child) => Align(
+                    alignment: _alignmentAnimation.value,
+                    child: RotationTransition(
+                      turns: _rotationAnimation,
+                      child: Image.asset(
+                        width: 85,
+                        height: 85,
+                        'assets/images/ball.png',
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ),
           ),
