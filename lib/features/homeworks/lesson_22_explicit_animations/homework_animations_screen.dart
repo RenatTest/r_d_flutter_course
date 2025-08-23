1545cadf68e793b4c6972a9ee025a9a9f1aace62
RenatTest <renattesttask@gmail.com>
2025-08-23 13:20:17 +0300

    feat(LAB-22-explicit-animations-homework): added ball image with background

--- lib/features/homeworks/lesson_22_explicit_animations/homework_animations_screen.dart
+++ lib/features/homeworks/lesson_22_explicit_animations/homework_animations_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class Homework22Screen extends StatelessWidget {
   const Homework22Screen({super.key});
@@ -11,7 +12,44 @@ class Homework22Screen extends StatelessWidget {
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
       ),
-      body: const SizedBox.shrink(),
+      body: Column(
+        children: [
+          Flexible(
+            flex: 9,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              color: HexColor('#90cdfa'),
+              alignment: Alignment.bottomCenter,
+              child: Image.asset(
+                width: 100,
+                height: 100,
+                'assets/images/ball.png',
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    HexColor('#96ca9b'),
+                    HexColor('#629e65'),
+                    HexColor('#3c7c41'),
+                    HexColor('#347636'),
+                    HexColor('#236a27'),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
