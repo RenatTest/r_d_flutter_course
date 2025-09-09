351694c2a397d28ae557530014d073e483a09c57
RenatTest <renattesttask@gmail.com>
2025-09-09 17:56:59 +0300

    feat(LAB-27-real-api-example-homework): catch errors with CustomServerError

--- lib/core/network/alerts_info_api/active_alerts_regions_api.dart
+++ lib/core/network/alerts_info_api/active_alerts_regions_api.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:r_d_flutter_course/features/alerts_info/active_alerts_regions/data/data_source/models/active_alerts_regions_dto/active_alerts_regions_dto.dart';
+import 'package:r_d_flutter_course/features/homeworks/lesson_23_error_handling_homework/data/repository/fake_user_repository.dart';
 
 abstract interface class ActiveAlertsRegionsApi {
   Future<ActiveAlertsRegionsDto> getActiveAlertsRegions();
@@ -12,17 +13,23 @@ class ActiveAlertsRegionsApiImpl implements ActiveAlertsRegionsApi {
 
   @override
   Future<ActiveAlertsRegionsDto> getActiveAlertsRegions() async {
-    final response = await dio.get<Map<String, dynamic>>(
-      '/v1/alerts/active.json',
-      queryParameters: {'token': _token},
-    );
+    try {
+      final response = await dio.get<Map<String, dynamic>>(
+        '/v1/alerts/active.json',
+        queryParameters: {'token': _token},
+      );
 
-    final data = response.data;
+      final data = response.data;
 
-    if (data == null) {
-      throw Exception('Failed to load top news, ${response.statusCode}');
+      if (data == null) {
+        throw Exception(
+          'Failed to load active alerts regions, ${response.statusCode}',
+        );
+      }
+
+      return ActiveAlertsRegionsDto.fromJson(data);
+    } catch (e) {
+      throw CustomServerError(errorMessage: e.toString());
     }
-
-    return ActiveAlertsRegionsDto.fromJson(data);
   }
 }
