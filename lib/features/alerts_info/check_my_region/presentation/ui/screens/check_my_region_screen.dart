b4049f2df1f63cd524622454031320e1bbef1c1d
RenatTest <renattesttask@gmail.com>
2025-09-10 17:10:21 +0300

    feat(LAB-27-real-api-example-homework): select region and receive alert data (setState)

--- lib/features/alerts_info/check_my_region/presentation/ui/screens/check_my_region_screen.dart
+++ lib/features/alerts_info/check_my_region/presentation/ui/screens/check_my_region_screen.dart
@@ -3,8 +3,46 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:r_d_flutter_course/features/alerts_info/check_my_region/presentation/cubit/check_my_region_cubit.dart';
 import 'package:r_d_flutter_course/features/alerts_info/check_my_region/presentation/cubit/check_my_region_state.dart';
 
-class CheckMyRegionScreen extends StatelessWidget {
+const regions = [
+  'Автономна Республіка Крим',
+  'Волинська область',
+  'Вінницька область',
+  'Дніпропетровська область',
+  'Донецька область',
+  'Житомирська область',
+  'Закарпатська область',
+  'Запорізька область',
+  'Івано-Франківська область',
+  'м. Київ',
+  'Київська область',
+  'Кіровоградська область',
+  'Луганська область',
+  'Львівська область',
+  'Миколаївська область',
+  'Одеська область',
+  'Полтавська область',
+  'Рівненська область',
+  'м. Севастополь',
+  'Сумська область',
+  'Тернопільська область',
+  'Харківська область',
+  'Херсонська область',
+  'Хмельницька область',
+  'Черкаська область',
+  'Чернівецька область',
+  'Чернігівська область',
+];
+
+class CheckMyRegionScreen extends StatefulWidget {
   const CheckMyRegionScreen({super.key});
+
+  @override
+  State<CheckMyRegionScreen> createState() => _CheckMyRegionScreenState();
+}
+
+class _CheckMyRegionScreenState extends State<CheckMyRegionScreen> {
+  String selectedRegion = regions.first;
+  int index = regions.indexOf('Автономна Республіка Крим');
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +55,7 @@ class CheckMyRegionScreen extends StatelessWidget {
         child: BlocBuilder<CheckMyRegionCubit, CheckMyRegionState>(
           builder: (context, state) {
             final alertData =
-                state.regionsAlerts?.regionsAlerts.toString()[10] ??
+                state.regionsAlerts?.regionsAlerts.toString()[index + 1] ??
                 'Немає даних';
             String alertMessage;
             Color alertColor;
@@ -51,13 +89,31 @@ class CheckMyRegionScreen extends StatelessWidget {
                   spacing: 10,
                   children: [
                     Text(
-                      'Мій регіон: м.Київ',
-                      style: TextStyle(
+                      'Мій регіон:',
+                      style: const TextStyle(
                         fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
+                    DropdownButton<String>(
+                      hint: const Text('Оберіть регіон'),
+                      value: selectedRegion,
+                      items: regions.map((String region) {
+                        return DropdownMenuItem<String>(
+                          value: region,
+                          child: Text(region),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        if (value != null) {
+                          setState(() {
+                            selectedRegion = value;
+                            index = regions.indexOf(selectedRegion);
+                          });
+                        }
+                      },
+                    ),
+
                     Text(
                       alertMessage,
                       style: TextStyle(
